abf7fb1d357306e3ccb7e2913ddc8ee0740a6b67
Jenny <jennykunte90@gmail.com>
2023-01-12 14:06:16 -0800

    added animation to letters

--- src/assets/components/Home/index.js
+++ src/assets/components/Home/index.js
@@ -1,15 +1,22 @@
 import LogoTitle from '../../images/logo-s.png';
 import { Link } from 'react-router-dom';
 import './index.scss';
-const Home =() => {
+import AnimatedLetters from '../AnimatedLetters';
+import { useState } from 'react'
 
+
+const Home =() => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['k','u','n','t','e']
+    const jobArray =['w','e','b',' ','d','e','v','e','l','o','p','e','r']
 
     return (
         <div className='container home-page'>
             <div className='text-zone'>
                 <h1>Hi,<br/> I'm
                 <img src={LogoTitle} alt='developer'/>
-                kunte
+                 <AnimatedLetters letterClass={letterClass} strArray={nameArray}
+                 idx={15}/>
                 <br />
                 web developer
                 </h1>
