e3d43bc8e3c77d3878c5e41a4787107949f1235b
Jenny <jennykunte90@gmail.com>
2023-01-12 17:23:43 -0800

    added bounce animation to letters

--- src/assets/components/Home/index.js
+++ src/assets/components/Home/index.js
@@ -13,12 +13,18 @@ const Home =() => {
     return (
         <div className='container home-page'>
             <div className='text-zone'>
-                <h1>Hi,<br/> I'm
+                <h1>
+                <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
                 <img src={LogoTitle} alt='developer'/>
                  <AnimatedLetters letterClass={letterClass} strArray={nameArray}
                  idx={15}/>
                 <br />
-                web developer
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray}
+                 idx={22}/>
                 </h1>
                 <h2>Junior Software Engineer / Project Managment </h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
