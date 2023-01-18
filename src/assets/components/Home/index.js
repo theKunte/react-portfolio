9e4ece1189163a76d9c1e8601f27ecb11b8cd41e
Jenny <jennykunte90@gmail.com>
2023-01-18 15:41:05 -0800

    bouncing effect

--- src/assets/components/Home/index.js
+++ src/assets/components/Home/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useState } from 'react'
+import { useEffect } from 'react';
 
 
 const Home =() => {
@@ -10,14 +11,21 @@ const Home =() => {
     const nameArray = ['k','u','n','t','e']
     const jobArray =['w','e','b',' ','d','e','v','e','l','o','p','e','r']
 
+    // TODO: Fix useEffect for bouncing letters min 40:50 in video
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //       setLetterClass('text-animate-hover')
+    //     }, 4000)
+    //   }, [])
+
     return (
         <div className='container home-page'>
             <div className='text-zone'>
                 <h1>
                 <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
-            <br />
-            <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _12`}>i,</span>
+                <br />
+                    <span className={`${letterClass} _13`}>I</span>
             <span className={`${letterClass} _14`}>'m</span>
                 <img src={LogoTitle} alt='developer'/>
                  <AnimatedLetters letterClass={letterClass} strArray={nameArray}
@@ -28,8 +36,6 @@ const Home =() => {
                 </h1>
                 <h2>Junior Software Engineer / Project Managment </h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
-
-
             </div>
         </div>
     )
