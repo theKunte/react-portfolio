32b63afd4454bc1bb78afc0d8e12607a2cf9f629
Jenny <jennykunte90@gmail.com>
2023-01-30 15:02:47 -0800

    updated About page and added svg spining cube

--- src/assets/components/About/index.js
+++ src/assets/components/About/index.js
@@ -1,14 +1,29 @@
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faCss3, faGitAlt, faHtml5, faJsSquare, faPython  } from '@fortawesome/free-brands-svg-icons';
 
 const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 3000)
+    
+        return () => clearTimeout(timer)
+      }, []) 
 
     return (
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
                     <AnimatedLetters
-                    strArray={['A','b','o','u','t',' ','m','e']}
+                    letterClass={letterClass}
+                    strArray={['A','b','o','u','t',' ','m','e',' ',':']}
                     idx={15}/>
                 </h1>
                 <p>
@@ -16,13 +31,36 @@ const About = () => {
                     I am very ambitious and excited to work with the latest technologies on interesting and diverse projects that improve
                     our lifes
                 </p>
-                <p  align="LEFT">>
+                <p  align="LEFT">
                     If I need to define myself in one sentence that would be world traveler, passionate baker, adventure hiker, photography enthusiast!
                 </p>
                 <p>
                     Add some more.
                 </p>
 
+            </div>
+
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faReact} color='#5ED4F4'/>
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faHtml5} color='#F06529'/>
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faCss3} color='#28A4D9'/>
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faJsSquare} color='#EFD81D'/>
+                    </div>
+                    <div className="face5">
+                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faPython} color='#28A4D9'/>
+                    </div>
+                </div>
             </div>
         </div>
     )
