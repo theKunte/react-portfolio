6d5cca9f13f3755b316de29ce8a500e51ac40eb5
Jenny <jennykunte90@gmail.com>
2023-02-03 15:06:24 -0800

    updated Portfolio section with title and animation. Encountered issue with displaying json data

--- src/assets/components/Portfolio/index.js
+++ src/assets/components/Portfolio/index.js
@@ -1,8 +1,54 @@
 import './index.scss';
 import React from "react";
+import AnimatedLetters from '../AnimatedLetters';
+import { useState, useEffect } from 'react';
+import portfolioData from '../../../data/portfolio.json';
 
 const Portfolio = () => {
-    return <div>Hello</div>
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+    console.log(portfolioData);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 3000);
+    
+        return () => clearTimeout(timer)
+      }, []) ;
+    
+
+    const renderPortfolio = (portfolio) => {
+        return (
+            <div className="images-container"> {
+                
+                portfolio.map((port, idx) => {
+                    return (
+                        <div className="image-box" key={idx}>
+                            <img 
+                                src={port.cover}
+                                className='portfolio-image'
+                                alt='portfolio'/>
+                         </div>       
+                        )
+                    })
+                }
+            </div>
+        );
+    }
+
+    return (
+        <div className='container portfolio-page'>
+            <h1 className='page-title'>
+            <AnimatedLetters
+                    letterClass={letterClass}
+                    strArray={['P','o','r','t','f','o','l','i','o',':']}
+                    idx={15}/>
+            </h1>
+            {/* TODO: fix renderPortfolio. Currently not displaying the image */}
+            {/* <div>{renderPortfolio(portfolioData)}</div> */}
+        </div>
+    )
 }
 
-export default Portfolio
+export default Portfolio;
