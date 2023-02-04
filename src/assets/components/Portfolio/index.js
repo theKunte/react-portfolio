e0bdcfe66f74f202b216f06e9b26cbd583d463d1
Jenny <jennykunte90@gmail.com>
2023-02-03 17:41:41 -0800

    added styling to portfolio

--- src/assets/components/Portfolio/index.js
+++ src/assets/components/Portfolio/index.js
@@ -29,6 +29,12 @@ const Portfolio = () => {
                                 src={port.cover}
                                 className='portfolio-image'
                                 alt='portfolio'/>
+                                <div className="content">
+                                    <p className="title">{port.name}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button className="btn"
+                                    onClick={() => window.open(port.url)}>View</button>
+                                </div>
                          </div>       
                         )
                     })
@@ -45,7 +51,7 @@ const Portfolio = () => {
                     strArray={['P','o','r','t','f','o','l','i','o',':']}
                     idx={15}/>
             </h1>
-?            <div>{renderPortfolio(portfolioData.portfolio)}</div>
+            <div>{renderPortfolio(portfolioData.portfolio)}</div>
         </div>
     )
 }
