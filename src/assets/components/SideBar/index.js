094db1e73b45806b0b5aca47b4201386c7d6743e
Jenny <jennykunte90@gmail.com>
2023-02-08 14:24:39 -0800

    updated styling for mobile view on all pages

--- src/assets/components/SideBar/index.js
+++ src/assets/components/SideBar/index.js
@@ -6,63 +6,72 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faHome,faUser,faEnvelope, faSuitcase, faBars, faClose} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { useState } from 'react';
+
+
 const Sidebar = () => {
-
     const [showNav, setShowNav] = useState(false);
-
+  
     return (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt='logo'/>
-            <img className='sub-logo' src={LogoSubtitle} alt='logo'/>
+      <div className="nav-bar">
+        <Link 
+          className="logo"
+          to="/"
+          onClick={() => setShowNav(false)}>
+          <img src={LogoS} alt="Logo" />
+          <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
         </Link>
         <nav className={showNav ? 'mobile-show' : ''}>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink activeclassname="active" className={"about-link"} to="/about" >
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink activeclassname="active" className={"contact-link"} to="/contact" >
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
-                <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e"/>
-            </NavLink>
-            <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} 
-            color="#ffd700" 
-            size="3x" 
-            className='close-icon'
-            />
+          <NavLink 
+            exact="true"
+            activeclassname="active"
+            to="/"
+            onClick={() => setShowNav(false)}>
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink 
+            activeclassname="active"
+            className="about-link"
+            to="/about"
+            onClick={() => setShowNav(false)}>
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            activeclassname="active"
+            className="portfolio-link"
+            to="/portfolio"
+            onClick={() => setShowNav(false)}
+          >
+            <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+          </NavLink>
+          <NavLink activeclassname="active" className="contact-link" to="/contact" onClick={() => setShowNav(false)}>
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+          <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} color="#ffd700" size="3x" className='close-icon' />
         </nav>
         <ul>
-            <li>
-                <a 
-                target="_blank" 
-                rel='noreferrer'
-                 href="https://www.linkedin.com/in/jenny-kunte-seattle/"
-                >
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
-                </a>
-            </li>
-            <li>
-                <a 
-                target="_blank" 
-                rel='noreferrer'
-                 href="https://github.com/theKunte"
-                >
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
-                </a>
-            </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/jenny-kunte-seattle/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"className="anchor-icon"/>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/theKunte"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e"className="anchor-icon"/>
+            </a>
+          </li>
         </ul>
-        <FontAwesomeIcon 
-        oncCkick={() => setShowNav(true)}
-        icon={faBars} 
-        color="#ffd700" 
-        size="3x" 
-        className='hamburger-icon'/>
-    </div>
+        <FontAwesomeIcon onClick={() => setShowNav(true)} icon={faBars} color="#ffd700" size="3x" className='hamburger-icon' />
+      </div>
     )
-}
-
-export default Sidebar;
+  }
+  
+  export default Sidebar
+  
