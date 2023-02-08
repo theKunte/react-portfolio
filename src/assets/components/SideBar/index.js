016469017000bfb38295e9c1c67212ceffa80803
Jenny <jennykunte90@gmail.com>
2023-02-07 18:25:49 -0800

    added responsive design for mobile user on for Navigation

--- src/assets/components/SideBar/index.js
+++ src/assets/components/SideBar/index.js
@@ -3,16 +3,20 @@ import {Link, NavLink} from 'react-router-dom'
 import LogoSubtitle from '../../images/logo_sub.png'
 import LogoS from '../../images/logo-s.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faHome,faUser,faEnvelope, faSuitcase} from '@fortawesome/free-solid-svg-icons'
+import {faHome,faUser,faEnvelope, faSuitcase, faBars, faClose} from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { useState } from 'react';
+const Sidebar = () => {
 
-const Sidebar = () => (
+    const [showNav, setShowNav] = useState(false);
+
+    return (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoS} alt='logo'/>
             <img className='sub-logo' src={LogoSubtitle} alt='logo'/>
         </Link>
-        <nav>
+        <nav className={showNav ? 'mobile-show' : ''}>
             <NavLink exact="true" activeclassname="active" to="/">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
             </NavLink>
@@ -25,7 +29,11 @@ const Sidebar = () => (
             <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
                 <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e"/>
             </NavLink>
-            
+            <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} 
+            color="#ffd700" 
+            size="3x" 
+            className='close-icon'
+            />
         </nav>
         <ul>
             <li>
@@ -47,7 +55,14 @@ const Sidebar = () => (
                 </a>
             </li>
         </ul>
+        <FontAwesomeIcon 
+        oncCkick={() => setShowNav(true)}
+        icon={faBars} 
+        color="#ffd700" 
+        size="3x" 
+        className='hamburger-icon'/>
     </div>
-)
+    )
+}
 
 export default Sidebar;
